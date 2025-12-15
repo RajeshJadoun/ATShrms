a0c35f42b93af4a8b5b663dfae6bd2b65c80d68e
Rajesh Jadoun <deo01@ntwoods.com>
2025-12-15 12:27:49 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
   API_URL: "PASTE_DEPLOYED_WEBAPP_URL",
-  GOOGLE_CLIENT_ID: "PASTE_CLIENT_ID.apps.googleusercontent.com"
+  GOOGLE_CLIENT_ID: "1029752642188-ku0k9krbdbsttj9br238glq8h4k5loj3.apps.googleusercontent.com"
 };
