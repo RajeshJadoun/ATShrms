a8ec2dcaafe58f6a2e1f3b23c2bad414798d616f
Rajesh Jadoun <deo01@ntwoods.com>
2025-12-15 12:32:51 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -23,7 +23,6 @@ export function logout() {
 export async function exchangeIdToken(idToken) {
   const res = await fetch(CONFIG.API_URL, {
     method: "POST",
-    mode: 'no-cors'
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ action: "LOGIN_EXCHANGE", token: "", data: { idToken } }),
   });
